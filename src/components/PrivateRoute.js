accccbc83ebbc00ce8768f475a063b5636e15f7a
hansfpc <hansfpc@outlook.com>
2018-07-13 11:43:02 -0400

    Update props handling - release 1.0.3

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,25 +2,40 @@ import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => {
+const PrivateRoute = ({
+  component: Component,
+  isAuthenticated,
+  redirectTo,
+  exact,
+  path,
+  strict,
+  ...rest
+}) => {
   return (
     <Route
       {...rest}
+      strict={strict ? strict : undefined}
+      exact={exact}
+      path={path}
       render={props =>
         isAuthenticated === true ? (
-          <Component {...props} {...rest} />
+          <Component {...{ ...props, ...rest }} />
         ) : (
-          <Redirect to={rest.redirectTo} />
+          <Redirect to={redirectTo} />
         )
       }
     />
   )
 }
 
-PrivateRoute.defaultProps = { redirectTo: '/login' }
+PrivateRoute.defaultProps = { redirectTo: '/login', strict: false, exact: false }
 
 PrivateRoute.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
+  strict: PropTypes.bool,
+  exact: PropTypes.bool,
+  component: PropTypes.func.isRequired,
+  path: PropTypes.string.isRequired,
   redirectTo: PropTypes.string.isRequired,
 }
 
